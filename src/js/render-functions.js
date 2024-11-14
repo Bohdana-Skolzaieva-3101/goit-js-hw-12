0f3cee1d7278be253b1046f783ee7df2ff7021e4
Bohdana Skolzaieva <skolzaeva2531@gmail.com>
2024-11-14 13:10:40 +0100

    part1

---
+++ src/js/render-functions.js
@@ -0,0 +1,37 @@
+export function renderGallery(images, append = false) {
+    const galleryContainer = document.querySelector('.gallery');
+    const markup = images.map(image => `
+    
+        <div class="image-card">
+            <a href="${image.largeImageURL}">
+                <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            </a>
+            <div class="image-info">
+                <span>💖 ${image.likes}</span>
+                <span>👀 ${image.views}</span>
+                <span>💬 ${image.comments}</span>
+                <span>📥 ${image.downloads}</span>
+            </div>
+        </div>
+    `).join('');
+
+    if (append) {
+        galleryContainer.insertAdjacentHTML('beforeend', markup);
+    } else {
+        galleryContainer.innerHTML = markup;
+    }
+
+}
+
+export function showError(message) {
+    if (typeof iziToast !== 'undefined') {
+        iziToast.error({
+            title: 'Error',
+            message: message,
+            position: 'topRight',
+            color: 'red',
+        });
+    } else {
+        console.error('iziToast is not defined');
+    }
+}
